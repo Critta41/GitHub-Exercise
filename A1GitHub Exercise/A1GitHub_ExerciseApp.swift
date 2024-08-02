0d8693f200b4a98fe493e15dc35f634c146ba452
Chris McGinley <chris@mcginley.com.au>
2024-08-02 13:13:41 +1000

    Initial Commit

---
+++ A1GitHub Exercise/A1GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  A1GitHub_ExerciseApp.swift
+//  A1GitHub Exercise
+//
+//  Created by Chris McGinley on 2/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct A1GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
